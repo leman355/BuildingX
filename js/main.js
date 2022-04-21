16163999d23f61fef35011eb4e5bd38244149fcf
leman355 <leman355@gmail.com>
2022-04-21 18:56:58 +0400

    h

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
-let header=document.querySelector("#header")
-let logo=document.querySelector("#header .headerBottom .logo")
-let iconBox=document.querySelector("#header .headerBottom .iconBox")
-let headerBottom=document.querySelector("#header .headerBottom")
+let header = document.querySelector("#header")
+let logo = document.querySelector("#header .headerBottom .logo")
+let iconBox = document.querySelector("#header .headerBottom .iconBox")
+let headerBottom = document.querySelector("#header .headerBottom")
 
-window.onscroll=function(){
-    if(window.scrollY>110){
-        header.style="position:relative"
+window.onscroll = function () {
+    if (window.scrollY > 125) {
+        header.style = "position:relative"
         header.classList.remove("headerTop")
         header.classList.remove("headerMiddle")
-        logo.style="opacity:1;visibility:visible;"
-        iconBox.style="opacity:1;visibility:visible;"
-        headerBottom.style="position:fixed; top:0; left:0"
+        logo.style = "opacity:1;visibility:visible;"
+        iconBox.style = "opacity:1;visibility:visible;"
+        headerBottom.style = "position:fixed; top:0; left:0"
     }
-    else{
-        logo.style="opacity:0;visibility:hidden;"
-        iconBox.style="opacity:0;visibility:hidden;"
-        headerBottom.style="position:relative"
+    else {
+        logo.style = "opacity:0;visibility:hidden;"
+        iconBox.style = "opacity:0;visibility:hidden;"
+        headerBottom.style = "position:relative"
     }
 }
+
+let management1 = document.querySelector(".construction .management1")
+let management2 = document.querySelector(".construction .management1 .management2")
+
+let a = 0
+setInterval(() => {
+    a++
+    if(a % 2==0){
+    management2.style = "opacity:0;visibility:hidden;"
+    }
+    else{
+        management2.style = "opacity:1;visibility:visible;"
+    }
+}, 2000);
